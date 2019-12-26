478acf31bb1d48f1220c09b1dc2c6c2e92a94c72
Jose Tafur <j-osea-nt@hotmail.com>
2019-12-25 21:46:54 -0500

    updating the views

--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,11 @@
 import requests
+from requests.compat import quote_plus
 from django.shortcuts import render
 from bs4 import BeautifulSoup
+from . import models
+
+BASE_CRAIGSLIST_URL = "https://lima.craigslist.org/search/?query={}"
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
 # Create your views here.
 def home(request):
@@ -8,8 +13,35 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
+    models.Search.objects.create(search=search)
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+
+    response = requests.get(final_url)
+    data = response.text
+
+    soup = BeutifulSoup(data, features='html.parser')
+    post_listings = soup.find_all('a', {'class': 'result-row'})
+
+    final_postings = []
+
+    for post in post_listings:
+        post_title = post.find(class='result-title').text
+        post_url = post.find('a').get('href')
+
+        if post.find(class='result-price'):
+            post_price = post.find(class='result-price').text
+        else:
+            post_price = 'No tiene precio'
+
+        if post.find(class='result-image').get('data-ids'):
+            post_image = post.find(class='result-image').get('data-ids')[0]
+
+        final_postings.append((post_title, post_url, post_price))
+
+
     # print(search)
     stuff_for_frontend = {
         'search': search,
+        'final postigns': final_postings
     }
     return render(request, 'my_app/new_search.html', stuff_for_frontend)
