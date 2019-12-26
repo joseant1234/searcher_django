10158cdac98f44519bcfbcdee927d9ceb2fda1a2
Jose Tafur <j-osea-nt@hotmail.com>
2019-12-26 14:55:23 -0500

    adding images in the results

--- my_app/views.py
+++ my_app/views.py
@@ -19,29 +19,30 @@ def new_search(request):
     response = requests.get(final_url)
     data = response.text
 
-    soup = BeutifulSoup(data, features='html.parser')
-    post_listings = soup.find_all('a', {'class': 'result-row'})
+    soup = BeautifulSoup(data, features='html.parser')
+    post_listings = soup.find_all('li', {'class': 'result-row'})
 
     final_postings = []
-
     for post in post_listings:
-        post_title = post.find(class='result-title').text
+        post_title = post.find(class_='result-title').text
         post_url = post.find('a').get('href')
 
-        if post.find(class='result-price'):
-            post_price = post.find(class='result-price').text
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
         else:
             post_price = 'No tiene precio'
 
-        if post.find(class='result-image').get('data-ids'):
-            post_image = post.find(class='result-image').get('data-ids')[0]
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_IMAGE_URL.format(post_image_id)
+        else:
+            post_image_url = 'https://craigslist.org/images/peace.jpg'
 
-        final_postings.append((post_title, post_url, post_price))
-
+        final_postings.append((post_title, post_url, post_price, post_image_url))
 
     # print(search)
     stuff_for_frontend = {
         'search': search,
-        'final postigns': final_postings
+        'final_postings': final_postings
     }
     return render(request, 'my_app/new_search.html', stuff_for_frontend)
